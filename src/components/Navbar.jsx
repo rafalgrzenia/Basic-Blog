139fe4814b55fea79bb75be038ea4b2d52ce6f27
Rafal Grzenia <rafalgrzenia96@gmail.com>
2024-01-08 13:03:31 +0100

    Add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav className="top-nav">
+      <div className="nav-text-large">My App</div>
+      <ul className="nav-list">
+        <li>
+          <Link to={"/posts"}>Posts</Link>
+        </li>
+        <li>
+          <Link to={"/users"}>Users</Link>
+        </li>
+        <li>
+          <Link to={"/todos"}>Todos</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
