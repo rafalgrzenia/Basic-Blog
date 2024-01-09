496d0f29f6c0131880258395e53c122ef9ba3c27
Rafal Grzenia <rafalgrzenia96@gmail.com>
2024-01-09 18:39:04 +0100

    Add getUser function

--- src/functions/users.js
+++ src/functions/users.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 
 export function getUsers(options) {
-    return axios.get("http://127.0.0.1:3000/users", options).then(res => res.data);
+  return axios
+    .get("http://127.0.0.1:3000/users", options)
+    .then((res) => res.data);
+}
+
+export function getUser(userId, options) {
+  return axios
+    .get(`http://127.0.0.1:3000/users/${userId}`, options)
+    .then((res) => res.data);
 }
