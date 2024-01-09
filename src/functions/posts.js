97810e3d843922967a2ec66437f99492ba93442c
Rafal Grzenia <rafalgrzenia96@gmail.com>
2024-01-09 17:42:31 +0100

    Add getPost function

--- src/functions/posts.js
+++ src/functions/posts.js
@@ -5,3 +5,7 @@ export function getPosts(options) {
     .get("http://127.0.0.1:3000/posts", options)
     .then((res) => res.data);
 }
+
+export function getPost(postId, options) {
+  return axios.get(`http://127.0.0.1:3000/posts/${postId}`, options).then(res => res.data);
+}
