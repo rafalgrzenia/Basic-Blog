5881e0c8ac86ba8c255c945aac80e1be9d836b78
Rafal Grzenia <rafalgrzenia96@gmail.com>
2024-01-10 12:26:43 +0100

    Add Fetch user data and display username

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,6 +1,6 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { getPost } from "../functions/Posts";
-// import { getUser } from "../functions/users";
+import { getUser } from "../functions/users";
 import { getComments } from "../functions/comments";
 
 // TODO:
@@ -8,12 +8,13 @@ import { getComments } from "../functions/comments";
 // On the Post page render out all of the comments for that post as well as the name for the user that created the post. Make the user name is a link to the user page.
 
 export function Post() {
-  const { post, comments } = useLoaderData();
+  const { post, comments, user } = useLoaderData();
+  console.log(user);
   return (
     <>
       <h1 className="page-title">{post.title}</h1>
-      <span className="page-subtitle">
-        {/* Add user request and display user's name */}
+      <span class="page-subtitle">
+        By: <Link to={`/users/${user.id}`}>{user.name}</Link>
       </span>
       <div>{post.body}</div>
       <h3 className="mt-4 mb-2">Comments</h3>
@@ -34,8 +35,8 @@ export function Post() {
 async function loader({ request: { signal }, params: { postId } }) {
   const comments = getComments(postId, { signal });
   const post = await getPost(postId, { signal });
-  // const userData = getUser(post.userId, { signal });
-  return { post, comments: await comments };
+  const user = getUser(post.userId, { signal });
+  return { post, comments: await comments, user: await user };
 }
 
 export const postRoute = {
