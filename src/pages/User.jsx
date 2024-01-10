0a6b7b091f8d1da11b499f4c3d6d4110b91da357
Rafal Grzenia <rafalgrzenia96@gmail.com>
2024-01-10 14:57:16 +0100

    Add posts section to User page

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,8 +1,9 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { getUser } from "../functions/users";
+import { getUserPosts } from "../functions/Posts";
 
 export function User() {
-  const user = useLoaderData();
+  const { user, posts } = useLoaderData();
 
   return (
     <>
@@ -16,16 +17,35 @@ export function User() {
           <b>Website:</b> {user.website}
         </div>
         <div>
-          <b>Address:</b> {user.address.street}, {user.address.suite}, {user.address.city}, {user.address.zipcode}
+          <b>Address:</b> {user.address.street}, {user.address.suite},{" "}
+          {user.address.city}, {user.address.zipcode}
         </div>
-        {/* Add Posts section later */}
+
+        <h3 className="mt-4 mb-2">Posts</h3>
+        <div className="card-grid">
+          {posts.map((post) => (
+            <div className="card" key={post.id}>
+              <div className="card-header">{post.title}</div>
+              <div className="card-body">
+                <div className="card-preview-text">{post.body}</div>
+              </div>
+              <div className="card-footer">
+                <Link className="btn" to={`/posts/${post.id}`}>
+                  View
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
 }
 
-function loader({ request: { signal }, params: { userId } }) {
-  return getUser(userId, { signal });
+async function loader({ request: { signal }, params: { userId } }) {
+  const user = getUser(userId, { signal });
+  const posts = getUserPosts(userId, { signal });
+  return { user: await user, posts: await posts };
 }
 
 export const userRoute = {
