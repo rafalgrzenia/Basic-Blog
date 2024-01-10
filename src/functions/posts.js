852b771052011f49de95a7cabad1a3bfd9341fdb
Rafal Grzenia <rafalgrzenia96@gmail.com>
2024-01-10 14:56:15 +0100

    Create getUsersPosts function

--- src/functions/posts.js
+++ src/functions/posts.js
@@ -10,3 +10,7 @@ export function getPosts(options) {
 export function getPost(postId, options) {
   return baseApi.get(`posts/${postId}`, options).then(res => res.data);
 }
+
+export function getUserPosts(userId, options) {
+  return baseApi.get(`posts?userId=${userId}`, options).then(res => res.data);
+}
