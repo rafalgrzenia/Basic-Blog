7d4d045003a9e257b6d3c4eca0200c6e948bcf9a
Rafal Grzenia <rafalgrzenia96@gmail.com>
2024-01-10 11:07:04 +0100

    Add loading state for all RootLayout children

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,11 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 
 export default function RootLayout() {
+  const { state } = useNavigation();
+  const loadingState = state === "loading";
+
+
   return (
     <>
       <Navbar />
-      <div className="container">
+      {loadingState && <div className="loading-spinner"></div>}
+      <div className={`${loadingState ? "loading" : ""} container`}>
         <Outlet />
       </div>
     </>
